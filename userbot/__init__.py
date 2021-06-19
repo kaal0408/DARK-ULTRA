6288ca8adc323a7f05704350db0812eb8f551a69
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-19 13:05:58 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -131,7 +131,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/DARK-COBRA/DARKCOBRA.git")
+    "https://github.com/kaal0408/darkultra.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
