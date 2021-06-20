743c5af4512b6d1062dddcc86fb5767028d74ec1
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-20 09:30:44 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [DARKCOBRA](https://www.github.com/hellboi-atul/hellboi-atul)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [DARKUltra](https://www.github.com/kaal0408)>> ",
 
          force_document=True,
 
