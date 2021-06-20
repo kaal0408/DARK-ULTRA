758d67f8c52bd214279252ca0b9e325db57c783d
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-20 09:28:50 +0530

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -102,7 +102,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbonised by [Dark-Cobra](https://github.com/DARK-COBRA/DARKCOBRA)",
+         caption="Carbonised by [Dark-Ultra](https://github.com/kaal0408/darkultra)",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
