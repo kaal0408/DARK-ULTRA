c5b44d08045ac8a9b9c1bb455f01e7c71115ce97
SHIVANSH <75574324+Hellboy-Shivansh@users.noreply.github.com>
2021-06-21 10:49:00 +0530

    Update start.py

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
 os.system("git clone https://github.com/kaal0408/darkultra darkultra")
-os.chdir("darkcobra")
+os.chdir("darkultra")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
