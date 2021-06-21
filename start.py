9e1179cf337b2e42de31c71e7683d946da8c8ad1
CrackHack10 <77793128+CrackHack10@users.noreply.github.com>
2021-06-21 18:47:03 +0530

    Test2 by Mr.Cracker

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from logging import DEBUG, INFO, basicConfig, getLogger, warning
 basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
 LOGS = getLogger("Helper")
 os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA darkcobra")
-os.chdir("darkultra")
+os.chdir("darkcobra")
 process = subprocess.Popen(
         ["python3", "-m", "userbot"],
         stdout=subprocess.PIPE,
