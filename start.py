d32f62b35282eff867d7cd7a4a29a0b42bd07107
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-22 08:58:41 +0530

    Done

--- start.py
+++ start.py
@@ -1,17 +1,6 @@
-import os 
-import subprocess
-from logging import DEBUG, INFO, basicConfig, getLogger, warning
-basicConfig(format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=INFO)
-LOGS = getLogger("Helper")
-os.system("git clone https://github.com/DARK-COBRA/DARKCOBRA darkcobra")
-os.chdir("darkcobra")
-process = subprocess.Popen(
-        ["python3", "-m", "userbot"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,)
-out, er = process.communicate()
-if er:
-    LOGS.warning(er.decode())
-print("::::::::::::::")
-if out:
-    LOGS.info(out.decode())
+from os import system
+#FO4 SOLBING SUSPENSION
+system("git clone https://github.com/kaal0408/darkultra.git && cd darkultra && python3 -m userbot")
+#TAKEN FROM LION
+#FOR DARK ULTRA 
+print("you got error please inform for this error in support group")
